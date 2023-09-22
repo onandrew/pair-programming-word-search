c50127c5f6ada17f5930e0670173f2e447ff4d82
Andrew On <a_on93@hotmail.com>
2023-09-22 14:47:31 -0400

    'created function for horizontal and vertical word search'

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+  if (word) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const transposedSearch  = transpose(letters);
+    const verticalJoin = transposedSearch.map(ls => ls.join(''));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) return true;
     }
+    for (l of verticalJoin) {
+      if (l.includes(word)) return true;
+    }
+  }
+  return false;
+};
+
+const transpose = (letters) => {
+  const transposed = []; 
+  if (letters.length > 0) {
+    for (let x = 0; x < letters[0].length; x++) {
+      transposed[x] = []; // creates a new array at transposed[0]
+      for (let y = 0; y < letters.length; y++) {
+        transposed[x].push(letters[y][x]);
+      }
+    }
+  }
+  return transposed;
 }
 
 module.exports = wordSearch
